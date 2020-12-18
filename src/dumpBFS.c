517e65e36a061779c907cb3c13c7351f73269200
antonionduarte <an.duarte@campus.fct.unl.pt>
2020-12-18 02:46:36 +0000

    DumpBFS formatting I think

--- src/dumpBFS.c
+++ src/dumpBFS.c
@@ -11,33 +11,32 @@ extern struct disk_operations disk_ops;
 extern struct super_operations super_ops;
 #endif
 
-
 #ifndef FFS_BYTEMAP_H
 #include "ffs_bytemap.h"
 extern struct bytemap_operations bmap_ops;
 #endif
-
 
 #ifndef FFS_INODE_H
 #include "ffs_inode.h"
 extern struct inode_operations inode_ops;
 #endif
 
-int main( int argc, char *argv[]) {
-
+int main(int argc, char *argv[]) {
   int ercode;
 
   struct super sb;
   struct bytemap bmapINODES, bmapDATA;
 
-  ercode= disk_ops.open(argv[1], 0);
+  ercode = disk_ops.open(argv[1], 0);
   if (ercode < 0) return ercode;
 
-  ercode=super_ops.read(&sb);
+  ercode = super_ops.read(&sb);
   if (ercode < 0) return ercode;
 
-  if (sb.fsmagic != FS_MAGIC)
-    { printf("** INVALID superblock, terminating...\n"); return 0; }
+  if (sb.fsmagic != FS_MAGIC) {
+    printf("** INVALID superblock, terminating...\n");
+    return 0;
+  }
   super_ops.print(&sb);
 
   // read the block of the inode bytemap
@@ -50,11 +49,11 @@ int main( int argc, char *argv[]) {
   printf("Bmap for data blocks:\n");
   bmap_ops.printTable(&bmapDATA);
 
-  ercode= inode_ops.printTable(sb.ninodeblocks, sb.ninodes, sb.startInArea);
+  ercode = inode_ops.printTable(sb.ninodeblocks, sb.ninodes, sb.startInArea);
   if (ercode < 0) return ercode;
 
-  for (int i= 0; i < sb.ninodes; i++) {
-    ercode= inode_ops.printFileData(sb.startInArea, i, sb.startDtArea);
+  for (int i = 0; i < sb.ninodes; i++) {
+    ercode = inode_ops.printFileData(sb.startInArea, i, sb.startDtArea);
     if (ercode < 0) return ercode;
   }
 
